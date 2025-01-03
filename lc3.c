6fe987de670f8d114bebe19cb8da1a5d1f7cf484
kswake <kbschin@gmail.com>
2025-01-02 21:30:54 -0500

    Update lc3.c

--- lc3.c
+++ lc3.c
@@ -250,7 +250,12 @@ int main(int argc, const char* argv[]){
                 break;
 
             case OP_STR:
-            //
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    uint16_t offset = sign_extend(instr & 0x3F, 6);
+                    mem_write(reg[r1] + offset, reg[r0]);
+                }
                 break;
 
             case OP_TRAP:
